2f59ee5e4b04ee1dd25c1418726e4da786837e66
Zidanni Clerigo <zclerigo40@stuy.edu>
2023-10-12 14:46:09 -0400

    thing

--- library/library/settings.py
+++ library/library/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'check_in',
+    'check_out',
+    'search',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'library.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR) + 'templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- library/search/views.py
+++ library/search/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+   titlePage = "Search Index"
+   gamesList = [{"name" : "Elder Scrolls: Oblivion", "Franchise" : True, "ethicallyAmbiguous" : False}, {"name" : "Furry Shades of Gray", "Franchise" : False, "ethicallyAmbiguous" : True}, {"name" : "Grand Theft Auto 6", "Franchise" : False, "ethicallyAmbiguous" : True}, {"name" : "Elden Ring", "Franchise" : True, "ethicallyAmbiguous" : False}, {"name" : "Minecraft", "Franchise" : True,  "ethicallyAmbiguous" : False}]
+   return render(request, "search/index.html")#context = {'titlePage' : titlePage,
+                                                #         'gamesList' : gamesList})
+   
+# def cookies(request):
+#    response = render(request, "games/cookies.html")
+#    response.set_cookie(key ="HI", value = "Bonjour")
+#    return response

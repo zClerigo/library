33d16a4655319e2980da02a39bff04eb0c2662da
ty-c1016 <tylerchan586@gmail.com>
2023-10-13 21:22:08 -0400

    wip

--- library/check_in/urls.py
+++ library/check_in/urls.py
@@ -6,10 +6,8 @@ app_name = "library"
 
 urlpatterns = [
    path("", views.index, name="index"),
-   # path('cookies/', views.cookies, name='cookies'),
-   # path('forms/', views.forms, name='forms'),
-   # path('book_info/', views.game_info, name='book_info'),
-   # path('new_forms/', views.new_forms, name='new_forms'),
-   # path('new_book_info/', views.new_game_info, name='new_book_info'),
+   path('cookies/', views.cookies, name='cookies'),
+   path('new_forms/', views.new_forms, name='new_forms'),
+
 
 ]
